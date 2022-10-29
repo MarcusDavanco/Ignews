72b7881c2c2786396076253e6e6dba3b0200fa8e
Marcus Davanco <md.daves@gmail.com>
2022-10-29 15:30:48 -0300

    feat: Autenticação com Next Auth

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,17 +1,29 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Marcus Davanço
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>

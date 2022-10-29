32f32f4cc9fc72e029ae36852a77f804bfcfb11a
Marcus Davanco <md.daves@gmail.com>
2022-10-29 20:07:47 -0300

    feat: Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -23,7 +23,19 @@ export default NextAuth({
       const { email } = user.user;
 
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              q.Match(q.Index("user_by_email"), q.Casefold(user.user.email))
+            )
+          )
+        );
         return true;
       } catch {
         return false;
